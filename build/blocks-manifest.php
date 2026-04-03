070843f15e605da0364351b348e527aa4eda98b4
Salim Shrestha <salimshrestha98@gmail.com>
2026-04-03 15:06:57 +0545

    Initial commit

---
+++ build/blocks-manifest.php
@@ -0,0 +1,25 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'quick-forms' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'create-block/quick-forms',
+		'version' => '0.1.0',
+		'title' => 'Quick Forms',
+		'category' => 'widgets',
+		'icon' => 'smiley',
+		'description' => 'Example block scaffolded with Create Block tool.',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'textdomain' => 'quick-forms',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css',
+		'viewScript' => 'file:./view.js'
+	)
+);
---
+++ build/quick-forms/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'baa2faa1a8a801f72d10');
